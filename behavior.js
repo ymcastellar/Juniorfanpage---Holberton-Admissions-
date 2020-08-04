a0173c565fb7e930ecee7f583fe32c1abd22aa7e
ymcastellar <yoycas@hotmail.com>
2020-08-04 18:30:54 -0500

    all file of the site

---
+++ behavior.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElement = document.getElementById("smart_thumbnail");
+	thumbnailElement.className = "small"
+
+
+thumbnailElement.addEventListener("click", function() {
+  
+  if (thumbnailElement.className == "small") {
+  	thumbnailElement.className = "";
+  } else {
+  	thumbnailElement.className = "small"
+
+  }
+});
+	
+
+	
+
+
+});
